407ef31ef81c725242cd541e9bf95b99462abca1
GabbyML <mgmontiitol@gmail.com>
2026-06-07 00:18:28 -0500

    Creación de la logica del juego

---
+++ src/games/even.js
@@ -0,0 +1,30 @@
+// definir que es un numero par
+const isEven = (number) => {
+    return (number % 2 === 0);
+}
+
+// dar numeros aleatorios .floor da un numero entero
+const randomNum = () => {
+    return Math.floor(Math.random() * 50);
+}
+
+// evaluacion de la respuesta del usuario
+const comparison = (answer, number) => {
+    return (answer === 'yes') === isEven(number);
+}
+// contador de las respuestas
+const game = () => {
+    let wins = 0;
+
+    while (wins < 3) {
+        const number = randomNum();
+        const answer = // getUserAnswer(number)
+        if (comparison(answer, number) === true){
+            wins++
+        } else {
+            wins = 0
+        }
+    }
+}
+
+export { isEven, randomNum, game };
