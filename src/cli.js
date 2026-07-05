ff26c3bf08b9f94be22c6067d7776a869de0a179
GabbyML <mgmontiitol@gmail.com>
2026-07-05 03:48:58 -0500

    creación del texto de pregunta para la calculadora

--- src/cli.js
+++ src/cli.js
@@ -3,15 +3,22 @@ import readlineSync from 'readline-sync';
 const bienvenido = () => {
     console.log ('Hola! Bienvenido a Juegos Mentales!');
     const name = readlineSync.question('Cómo quieres que te llame?');
-    console.log (`Hola! ${name}`)
+    console.log (`Hola! ${name}`);
     return name;
 }
 
 const pregunta = (number) => {
     console.log ('Responde "yes" si el número es par, de lo contrario responde "no".');
-    console.log (`Pregunta: ${number}`)
-    const respuesta = readlineSync.question ('Tu respuesta: ')
+    console.log (`Pregunta: ${number}`);
+    const respuesta = readlineSync.question ('Tu respuesta: ');
     return respuesta;
 }
+
+const preguntaCalc = (num1, num2, sign) =>{
+    console.log (`Pregunta: ${num1} ${sign} ${num2}`);
+    const respuesta = readlineSync.question ('Tu respuesta: ');
+    return respuesta;
+}
+
 export default bienvenido;
-export { pregunta };
+export { pregunta, preguntaCalc };
--- src/games/calc.js
+++ src/games/calc.js
@@ -19,3 +19,18 @@ const operation = (num1, num2, sign) => {
         return num1 * num2;
     }
 }
+
+const game = (name) => {
+    let wins = 0;
+    while (wins < 3){
+        const rightAnswer = operation();
+        const userAnswer = pregunta(number);
+        if (Number(userAnswer) === rightAnswer){
+            wins++;
+            console.log('¡Muy Bien, Es Correcto!')
+        } else {
+            wins = 0
+            console.log(`'${userAnswer}' es una respuesta incorrecta. La respuesta correcta era '${rightAnswer}'. ¡Intentémoslo de nuevo, ${name}!`)
+        }
+    }
+}
