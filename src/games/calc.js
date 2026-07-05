b3663f8b34d1fd8d6c8b52088ebb2e1c0fd4128e
GabbyML <mgmontiitol@gmail.com>
2026-07-05 03:58:08 -0500

    se completó la funcion logica  game de la calculadora

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,3 +1,4 @@
+import {preguntaCalc} from '../cli.js';
 
 //signos aleatorios
 const randomSign = () => {
@@ -22,9 +23,14 @@ const operation = (num1, num2, sign) => {
 
 const game = (name) => {
     let wins = 0;
+    console.log('¿Cuál es el resultado de la expresión?');
     while (wins < 3){
-        const rightAnswer = operation();
-        const userAnswer = pregunta(number);
+        const num1 = randomNum();
+        const num2 = randomNum();
+        const sign = randomSign();
+
+        const rightAnswer = operation(num1, num2, sign);
+        const userAnswer = preguntaCalc(num1, num2, sign);
         if (Number(userAnswer) === rightAnswer){
             wins++;
             console.log('¡Muy Bien, Es Correcto!')
@@ -33,4 +39,7 @@ const game = (name) => {
             console.log(`'${userAnswer}' es una respuesta incorrecta. La respuesta correcta era '${rightAnswer}'. ¡Intentémoslo de nuevo, ${name}!`)
         }
     }
+    console.log (`¡Felicidades, ${name} Ganaste!`)
 }
+
+export { game };
