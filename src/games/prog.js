efa963b1d8ad4d8bbfe76b24d35e43620bce86c1
GabbyML <mgmontiitol@gmail.com>
2026-07-06 04:21:01 -0500

    se completo la logica del juego de progresion

--- src/games/prog.js
+++ src/games/prog.js
@@ -1,4 +1,6 @@
 
+import {preguntaprog} from '../cli.js';
+
 const randomNum = () => {
     return Math.floor(Math.random() * (50 - 1) + 1);
 }
@@ -21,3 +23,24 @@ const hiddenIndex = (progression) => {
     progression[index] = '..';
     return [index, guess];
 }
+
+const game = (name) => {
+    let wins = 0;
+    console.log('¿Qué número falta en la progresión?');
+    while (wins < 3){
+        const coll = progression(); 
+        const [ _, rightAnswer] = hiddenIndex(coll);
+        const userAnswer = preguntaprog(coll);
+        
+        if (Number(userAnswer) === rightAnswer){
+            wins++;
+            console.log('¡Muy Bien, Es Correcto!')
+        } else {
+            wins = 0
+            console.log(`'${userAnswer}' es una respuesta incorrecta. La respuesta correcta era '${rightAnswer}'. ¡Intentémoslo de nuevo, ${name}!`)
+        }
+    }
+    console.log (`¡Felicidades, ${name} Ganaste!`)
+}
+
+export {game};
