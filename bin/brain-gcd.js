b7416fbc59f4551f164187bdd2ebadeb0eff7d17
GabbyML <mgmontiitol@gmail.com>
2026-07-05 20:33:53 -0500

    correcion de directorio de importación

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import bienvenido from '../src/cli.js';
-import { gcd } from '../src/games/brain-gcd.js';
+import { gcd } from '../src/games/gcd.js';
 
 const name = bienvenido();
 gcd(name);
