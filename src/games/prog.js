25a30ccff6f4cd561048522d1010f4b6316a1e2b
GabbyML <mgmontiitol@gmail.com>
2026-07-05 23:53:47 -0500

    creación de la fucion hiddenIndex

--- src/games/prog.js
+++ src/games/prog.js
@@ -3,10 +3,21 @@ const randomNum = () => {
     return Math.floor(Math.random() * (50 - 1) + 1);
 }
 
+const randomIndex = () => {
+    return Math.floor(Math.random() * 10);
+}
+
 const progression = () => {
     const length = 10;
     const start = randomNum();
     const step = randomNum();
     const number = Array.from({ length }, (_ , index) => start + index * step);
     return number;
+}
+
+const hiddenIndex = (progression) => {
+    const index = randomIndex();
+    const guess = progression[index];
+    progression[index] = '..';
+    return [index, guess];
 }
