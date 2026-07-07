d5847f6d00f1d6c06e019c921368d04adc78b2c5
GabbyML <mgmontiitol@gmail.com>
2026-07-07 01:23:20 -0500

    cambio en la pregunta para el nombre de bienvenida

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const bienvenido = () => {
     console.log ('¡Bienvenido a Brain Games!');
-    const name = readlineSync.question('Cómo quieres que te llame?');
+    const name = readlineSync.question('¿Cuál es tu nombre? ');
     console.log (`Hola! ${name}`);
     return name;
 }
