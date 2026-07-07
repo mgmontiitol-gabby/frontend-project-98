fcd922c0b3ff711da8e0b0b8be96a7448eacd9b1
GabbyML <mgmontiitol@gmail.com>
2026-07-07 01:08:50 -0500

    correccion mensaje de bienvenida

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const bienvenido = () => {
-    console.log ('Hola! Bienvenido a Juegos Mentales!');
+    console.log ('¡Bienvenido a Brain Games!');
     const name = readlineSync.question('Cómo quieres que te llame?');
     console.log (`Hola! ${name}`);
     return name;
