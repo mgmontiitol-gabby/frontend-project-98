1e973c512f9f326aef389316491c3bfc2bdc69b3
GabbyML <mgmontiitol@gmail.com>
2026-07-07 01:26:42 -0500

    correcion en en la impresion de nombre

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const bienvenido = () => {
     console.log ('¡Bienvenido a Brain Games!');
     const name = readlineSync.question('¿Cuál es tu nombre? ');
-    console.log (`Hola! ${name}`);
+    console.log (`¡Hola, ${name}!`);
     return name;
 }
 
