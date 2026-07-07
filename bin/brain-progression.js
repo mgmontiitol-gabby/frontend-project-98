2b6641e88f0bc6230f30d7b45fd19bc880832979
GabbyML <mgmontiitol@gmail.com>
2026-07-07 02:07:51 -0500

    correcion logica de juego y nombre de los archivos

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import bienvenido from '../src/cli.js';
-import { game } from '../src/games/prog.js';
+import { game } from '../src/games/progression.js';
 
 const name = bienvenido();
 game(name);
