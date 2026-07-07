034f58a0ac552601f95b3a53d9dec72a1a57b025
GabbyML <mgmontiitol@gmail.com>
2026-07-07 01:43:47 -0500

    correcion de logica para romperse despues de falla del usuario

--- src/games/even.js
+++ src/games/even.js
@@ -18,18 +18,23 @@ const comparison = (answer, number) => {
 // contador de las respuestas
 const game = (name) => {
     let wins = 0;
+    let failed = false;
     while (wins < 3) {
         const number = randomNum();
         const answer = pregunta(number);
+        
         if (comparison(answer, number) === true){
             wins++
             console.log('¡Correcto!');
         } else {
-            wins = 0
             console.log(`'${answer}' es una respuesta incorrecta. La respuesta correcta era '${isEven(number) ? "yes" : "no"}'. ¡Intentémoslo de nuevo, ${name}!`)
+            failed = true
+            break;
         }
     }
-    console.log (`¡Felicidades, ${name}!`)
+    if (!failed) {
+    console.log(`¡Felicidades, ${name}!`);
+    }
 }
 
 export { isEven, randomNum, game };
